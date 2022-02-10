8b3855b7aa3db248bb7637dea04e1bb3363b5926
Vlad Sosnov <vladyslav.sosnov@gmail.com>
2022-02-10 11:53:27 +0100

    feat(init project): add main dependencies and settings

---
+++ src/components/Tag/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+interface TagProps {
+  title?: string;
+}
+const Tag: FC<TagProps> = ({ title = "My title" }) => {
+  return <div style={{ backgroundColor: "yellow" }}>{title}</div>;
+};
+export default Tag;
